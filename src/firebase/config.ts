efa8f7f711325755e79cb691ed1217120e954134
PNPtv Support <support@pnptv.app>
2025-10-07 16:43:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9933426702-65d9f",
+  "appId": "1:828697823895:web:c3ccc0892e9dc54006b7d2",
+  "apiKey": "AIzaSyDPE1FcvNuIv2JVbqqeuFCk3blDExQd3_g",
+  "authDomain": "studio-9933426702-65d9f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "828697823895"
+};
